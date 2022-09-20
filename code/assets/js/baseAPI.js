feaed009dd34e220110ae2b2a1a9cf1298b33cae
liuwenjun <2781364967@qq.com>
2022-09-20 15:37:32 +0800

    完成首页功能

--- code/assets/js/baseAPI.js
+++ code/assets/js/baseAPI.js
@@ -1,3 +1,15 @@
 $.ajaxPrefilter(function (options) {
-  options.url += "http://www.liulongbin.top:3007";
+  options.url = "http://www.liulongbin.top:3007" + options.url;
+  if (options.url.indexOf("/my/") !== -1) {
+    options.headers = {
+      Authorization: localStorage.getItem("token") || "",
+    };
+  }
+  options.complete = function (res) {
+    console.log(res);
+    if (res.responseJSON.status == 1) {
+      location.href = "./login.html";
+      localStorage.removeItem("token");
+    }
+  };
 });
---
+++ code/assets/js/index.js
@@ -0,0 +1,50 @@
+$(function () {
+  getUserInfo();
+  var layer = layui.layer;
+  $("#btnLogOut").on("click", function () {
+    layer.confirm(
+      "确定退出登录?",
+      { icon: 3, title: "提示" },
+      function (index) {
+        //do something
+        localStorage.removeItem("token");
+        location.href = "./login.html";
+
+        layer.close(index);
+      }
+    );
+  });
+});
+// 获取用户信息函数
+function getUserInfo() {
+  $.ajax({
+    method: "GET",
+    url: "/my/userinfo",
+    // headers: { Authorization: localStorage.getItem("token") || "" },
+    success: function (res) {
+      if (res.status !== 0) {
+        return layui.layer.msg("获取用户信息失败!");
+      }
+      renderAvater(res.data);
+    },
+    // complete: function (res) {
+    //   console.log(res);
+    //   if (res.responseJSON.status == 1) {
+    //     localStorage.removeItem("token");
+    //     location.href = "./login.html";
+    //   }
+    // },
+  });
+}
+// 渲染头像函数
+function renderAvater(user) {
+  var name = user.nickname || user.username;
+  $("#welcome").html("欢迎&nbsp;&nbsp;" + name);
+  if (user.user_pic) {
+    $(".layui-nav-img").attr("src", user.user_pic).show();
+    $(".text_avatar").hide();
+  } else {
+    $(".layui-nav-img").hide();
+    $(".text_avatar").html(name[0].toUpperCase()).show();
+  }
+}
